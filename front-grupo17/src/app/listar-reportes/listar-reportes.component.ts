8ef3bdd2612438ac9398e7ea2e8610c026d4e74a
wendychamale <58566059+wendychamale@users.noreply.github.com>
2021-09-30 23:15:13 -0600

    Servicio buscar, listar reporte

--- front-grupo17/src/app/listar-reportes/listar-reportes.component.ts
+++ front-grupo17/src/app/listar-reportes/listar-reportes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MainComponent } from '../main/main.component'
+import { ServicioService } from 'src/app/servicios/servicio.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-reportes',
@@ -8,12 +10,44 @@ import { MainComponent } from '../main/main.component'
 })
 export class ListarReportesComponent implements OnInit {
 
-  constructor(private mainComponent: MainComponent) {
+  constructor(private mainComponent: MainComponent,public service: ServicioService, public router: Router) {
     this.mainComponent.listReportsActive();
   }
 
   ngOnInit(): void {
+    this.vertodo();
   }
 buscarc:string="";
-  buscar(){}
+Reporte = [];
+
+
+
+  buscar(){
+    console.log(this.buscarc)
+    this.service.Buscar(this.buscarc)
+
+    .subscribe(
+      res => {
+       this.Reporte=res;
+      },
+      err => console.log(err)
+    )
+
+  }
+
+
+  vertodo(){
+    this.service.getReporte()
+
+    .subscribe(
+      res => {
+       this.Reporte=res;
+      },
+      err => console.log(err)
+    )
+
+  }
+
+
+  
 }
---
+++ front-grupo17/src/app/servicios/servicio.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicioService {
+
+  constructor(    private http: HttpClient) { }
+  api1 = 'http://localhost:3001/s1'
+
+  getReporte()
+  {
+    return this.http.get<any>(`${this.api1}/reporte/obtenerTodos`);
+  }
+
+  Buscar(carnet:any){
+    return this.http.post<any>(`${this.api1}/reporte/obtener`,carnet);
+  }
+}
